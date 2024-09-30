12a71a65483c400e50df9f37f7abef340916c9bd
Chris <chrisn5413@gmail.com>
2024-09-30 13:46:47 -0700

    completed step 5 to play a single round. Also made user input case insensitive

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -29,13 +29,54 @@ function getComputerChoice() {
 function getHumanChoice() {
     let trys = 3;
     let userChoice = prompt('Lets play rock, paper, scissors. What option will you choose?','rock');
-    
+    userChoice = userChoice.toLowerCase();
+
     while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
         userChoice = prompt('Sorry, invalid choice. Please choose: "rock", "paper", or "scissors"');
+        userChoice = userChoice.toLowerCase();
     }
 
-    log('userChoice =',userChoice);
+    // log('userChoice =',userChoice);
     
     return userChoice;
+}
 
+// plays a round of rock paper scissors
+// parameters: humanChoice, computerChoice
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        log('Its a tie. Both players chose', humanChoice);
+    }
+    else if (humanChoice === 'rock'){
+        if (computerChoice === 'scissors'){
+            log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else {
+            log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }   
+    }
+    else if (humanChoice === 'paper'){
+        if (computerChoice === 'rock'){
+            log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;    
+        }
+        else {
+            log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    }
+    else if (humanChoice === 'scissors'){
+        if (computerChoice === 'paper'){
+            log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else {
+            log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        } 
+    }
+    else
+        throw HowDidYouGetHere('This else should never run');
 }
